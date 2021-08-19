efaad685701e6c10898165b8c363a2de081fd7e3
Cai <trieungochai.dev@gmail.com>
2021-08-19 15:37:59 +0700

    Basic Form Elements

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,26 @@
 import ReactDOM from "react-dom";
+import { useState } from "react";
 
 const App = () => {
-  return <div>See you Space Cowboy!!!</div>;
+  const [input, setInput] = useState("");
+  const [code, setCode] = useState("");
+
+  const onClick = () => {
+    console.log(input);
+  };
+
+  return (
+    <div>
+      <textarea
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
