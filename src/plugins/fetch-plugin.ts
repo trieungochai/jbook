1573b9aae8bb65f64690bb4f130edccd24687563
Cai <trieungochai.dev@gmail.com>
2021-08-21 19:39:02 +0700

    Separate Load Filters

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -10,44 +10,65 @@ export const fetchPlugin = (inputCode: string) => {
   return {
     name: "fetch-plugin",
     setup(build: esbuild.PluginBuild) {
-      build.onLoad({ filter: /.*/ }, async (args: any) => {
-        if (args.path === "index.js") {
-          return {
-            loader: "jsx",
-            contents: inputCode,
-          };
-        }
+      build.onLoad({ filter: /(^index\.js$)/ }, () => {
+        return {
+          loader: "jsx",
+          contents: inputCode,
+        };
+      });
 
+      build.onLoad({ filter: /.css$/ }, async (args: any) => {
         // Check to see if we have already fetched this file and if it is in the cache
-        // const cacheResult = await fileCache.getItem<esbuild.OnLoadResult>(
-        //   args.path
-        // );
+        const cacheResult = await fileCache.getItem<esbuild.OnLoadResult>(
+          args.path
+        );
 
         // If it is, return its immediately
-        // if (cacheResult) {
-        //   return cacheResult;
-        // }
+        if (cacheResult) {
+          return cacheResult;
+        }
 
         const { data, request } = await axios.get(args.path);
 
-        const fileType = args.path.match(/.css$/) ? "css" : "jsx";
         const escaped = data
           .replace(/\n/g, "")
           .replace(/"/g, '\\"')
           .replace(/'/g, "\\'");
-        const contents =
-          fileType === "css"
-            ? `
+        const contents = `
             const style = document.createElement('style');
             style.innerText = '${escaped}';
             document.head.appendChild(style);
         
-        `
-            : data;
+        `;
 
         const result: esbuild.OnLoadResult = {
           loader: "jsx",
           contents,
+          resolveDir: new URL("./", request.responseURL).pathname,
+        };
+
+        //store response in cache
+        await fileCache.setItem(args.path, result);
+
+        return result;
+      });
+
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
+        // Check to see if we have already fetched this file and if it is in the cache
+        const cacheResult = await fileCache.getItem<esbuild.OnLoadResult>(
+          args.path
+        );
+
+        // If it is, return its immediately
+        if (cacheResult) {
+          return cacheResult;
+        }
+
+        const { data, request } = await axios.get(args.path);
+
+        const result: esbuild.OnLoadResult = {
+          loader: "jsx",
+          contents: data,
           resolveDir: new URL("./", request.responseURL).pathname,
         };
 
