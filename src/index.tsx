d38fb4987e324274d9b6b3231b841f360e9a3271
Cai <trieungochai.dev@gmail.com>
2021-08-22 15:30:54 +0700

    Passing Code to the IFrame

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
 
 const App = () => {
   const ref = useRef<any>();
+  const iframe = useRef<any>();
   const [input, setInput] = useState("");
   const [code, setCode] = useState("");
 
@@ -35,18 +36,23 @@ const App = () => {
       },
     });
 
-    // console.log(result);
-
-    setCode(result.outputFiles[0].text);
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, "*");
   };
 
   const html = `
-    <script>
-      ${code}
-      <script></script>
-    </script>
+    <html>
+      <head></head>
+      <body>
+        <div id="root"></div>
+        <script>
+          window.addEventListener('message', (event) => {
+            eval(event.data);
+          }, false);
+        </script>
+      </body>
+    </html>
   `;
-  
+
   return (
     <div>
       <textarea
@@ -57,7 +63,7 @@ const App = () => {
         <button onClick={onClick}>Submit</button>
       </div>
       <pre>{code}</pre>
-      <iframe sandbox="allow-scripts" srcDoc={html} />
+      <iframe ref={iframe} sandbox="allow-scripts" srcDoc={html} />
     </div>
   );
 };
