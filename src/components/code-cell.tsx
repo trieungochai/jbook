bef11998cf0f209043746b0030c5487504a377d6
Cai <trieungochai.dev@gmail.com>
2021-08-24 01:10:06 +0700

    Applying Horizontal Resize Props

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -8,11 +8,6 @@ const CodeCell = () => {
   const [code, setCode] = useState("");
   const [input, setInput] = useState("");
 
-  // const onClick = async () => {
-  //   const output = await bundle(input);
-  //   setCode(output);
-  // };
-
   return (
     <Resizable direction="vertical">
       <div style={{ height: "100%", display: "flex", flexDirection: "row" }}>
