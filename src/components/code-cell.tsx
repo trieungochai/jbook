f7f3f348eccca09f49c9d8b864c96f4a3723237d
Cai <trieungochai.dev@gmail.com>
2021-08-23 23:59:22 +0700

    Setting a Resize Hanlde

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -8,21 +8,18 @@ const CodeCell = () => {
   const [code, setCode] = useState("");
   const [input, setInput] = useState("");
 
-  const onClick = async () => {
-    const output = await bundle(input);
-    setCode(output);
-  };
+  // const onClick = async () => {
+  //   const output = await bundle(input);
+  //   setCode(output);
+  // };
 
   return (
     <Resizable direction="vertical">
-      <div>
+      <div style={{ height: "100%", display: "flex", flexDirection: "row" }}>
         <CodeEditor
           initialValue="const a = 1;"
           onChange={(value) => setInput(value)}
         />
-        <div>
-          <button onClick={onClick}>Submit</button>
-        </div>
         <Preview code={code} />
       </div>
     </Resizable>
--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -1,11 +1,15 @@
 import { ResizableBox } from "react-resizable";
-
+import "./resizable.css";
 interface ResizableProps {
   direction: "horizontal" | "vertical";
 }
 
 const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
-  return <div>{children}</div>;
+  return (
+    <ResizableBox height={300} width={Infinity} resizeHandles={["s"]}>
+      {children}
+    </ResizableBox>
+  );
 };
 
 export default Resizable;
