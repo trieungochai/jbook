a3828b1490f883ca3ed1e2929afe343215ae163d
Cai <trieungochai.dev@gmail.com>
2021-08-24 22:00:01 +0700

    On Resize Events

--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -1,11 +1,23 @@
 import "./resizable.css";
 import { ResizableBox, ResizableBoxProps } from "react-resizable";
+import { useEffect } from "react";
 interface ResizableProps {
   direction: "horizontal" | "vertical";
 }
 
 const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
   let resizableProps: ResizableBoxProps;
+
+  useEffect(() => {
+    const listener = () => {
+      console.log(window.innerWidth, window.innerHeight);
+    };
+    window.addEventListener("resize", listener);
+
+    return () => {
+      window.removeEventListener("resize", listener);
+    };
+  }, []);
 
   if (direction === "horizontal") {
     resizableProps = {
