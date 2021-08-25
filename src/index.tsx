d62fc8ec2b3bb92cf61b0feeca685205c30673b7
Cai <trieungochai.dev@gmail.com>
2021-08-25 16:21:10 +0700

    Toggling the Markdown Editor

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import ReactDOM from "react-dom";
 import "bulmaswatch/superhero/bulmaswatch.min.css";
 import CodeCell from "./components/code-cell";
+import TextEditor from "./components/text-cell";
 
 const App = () => {
   return (
     <div>
-      <CodeCell />
+      <TextEditor />
     </div>
   );
 };
