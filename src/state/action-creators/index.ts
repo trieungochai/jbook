89f0442c91773ecb73e5c8e1145b0fff2ba9a0f8
Cai <trieungochai.dev@gmail.com>
2021-08-27 01:50:48 +0700

    Defining Direction

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -7,6 +7,7 @@ import {
   InsertCellBeforeAction,
 } from "../actions";
 import { CellTypes } from "../cell";
+import { Direction } from "../actions";
 
 export const updateCell = (id: string, content: string): UpdateCellAction => {
   return {
@@ -25,10 +26,7 @@ export const deleteCell = (id: string): DeleteCellAction => {
   };
 };
 
-export const moveCell = (
-  id: string,
-  direction: "up" | "down"
-): MoveCellAction => {
+export const moveCell = (id: string, direction: Direction): MoveCellAction => {
   return {
     type: ActionType.MOVE_CELL,
     payload: {
