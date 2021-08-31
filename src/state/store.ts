a32af776c48ec798d65881515eb6cd5f4dad6eb2
Cai <trieungochai.dev@gmail.com>
2021-08-31 23:22:11 +0700

    Cell Type Rendering

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,26 +1,38 @@
 import { applyMiddleware, createStore } from "redux";
 import thunk from "redux-thunk";
-// import { ActionType } from "./action-types";
+import { ActionType } from "./action-types";
 import reducers from "./reducers";
 
 export const store = createStore(reducers, {}, applyMiddleware(thunk));
 
-// store.dispatch({
-//   type: ActionType.INSERT_CELL_BEFORE,
-//   payload: {
-//     id: null,
-//     type: "code",
-//   },
-// });
+store.dispatch({
+  type: ActionType.INSERT_CELL_BEFORE,
+  payload: {
+    id: null,
+    type: "code",
+  },
+});
 
-// store.dispatch({
-//   type: ActionType.INSERT_CELL_BEFORE,
-//   payload: {
-//     id: null,
-//     type: "text",
-//   },
-// });
+store.dispatch({
+  type: ActionType.INSERT_CELL_BEFORE,
+  payload: {
+    id: null,
+    type: "text",
+  },
+});
 
-// const id = store.getState().cells?.order[0];
+store.dispatch({
+  type: ActionType.INSERT_CELL_BEFORE,
+  payload: {
+    id: null,
+    type: "code",
+  },
+});
 
-// console.log(store.getState(), id);
+store.dispatch({
+  type: ActionType.INSERT_CELL_BEFORE,
+  payload: {
+    id: null,
+    type: "text",
+  },
+});
