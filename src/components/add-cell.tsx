f84e1e720da9e932965464033d0130dfb0e98211
Cai <trieungochai.dev@gmail.com>
2021-09-02 13:36:33 +0700

    Refactoring the Redux Store

--- src/components/add-cell.tsx
+++ src/components/add-cell.tsx
@@ -7,14 +7,14 @@ interface AddCellProps {
 }
 
 const AddCell: React.FC<AddCellProps> = ({ nextCellId, forceVisible }) => {
-  const { insertCellBefore } = useActions();
+  const { insertCellAfter } = useActions();
 
   return (
     <div className={`add-cell ${forceVisible} && 'forceVisible'}`}>
       <div className="add-buttons">
         <button
           className="button is-rounded is-primary is-small"
-          onClick={() => insertCellBefore(nextCellId, "code")}
+          onClick={() => insertCellAfter(nextCellId, "code")}
         >
           <span className="icon is-small">
             <i className="fas fa-plus" />
@@ -23,7 +23,7 @@ const AddCell: React.FC<AddCellProps> = ({ nextCellId, forceVisible }) => {
         </button>
         <button
           className="button is-rounded is-primary is-small"
-          onClick={() => insertCellBefore(nextCellId, "text")}
+          onClick={() => insertCellAfter(nextCellId, "text")}
         >
           <span className="icon is-small">
             <i className="fas fa-plus" />
